75a302f7f465c163d739115a2da5169d5c300dd5
edgar.pech <edgar.pech@workiasolutions.com>
2023-05-17 19:47:58 -0600

    add active model

---
+++ src/api/active-model/controllers/active-model.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * active-model controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::active-model.active-model');
