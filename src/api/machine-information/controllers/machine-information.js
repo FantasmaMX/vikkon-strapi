f7823d7523feaeeb14dae467e845b8474bdc4b76
edgar.pech <edgar.pech@workiasolutions.com>
2023-05-24 21:59:10 -0600

    add information machine

---
+++ src/api/machine-information/controllers/machine-information.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * machine-information controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::machine-information.machine-information');
---
+++ src/api/machine-information/routes/machine-information.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * machine-information router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::machine-information.machine-information');
